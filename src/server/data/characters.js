59976ce136d9dc0d6da508bc3c27c062fb69ffa8
mikhail-riabokon <mikhail.riabokon@gmail.com>
2015-11-12 23:24:14 +0200

    change characters

--- src/server/data/characters.js
+++ src/server/data/characters.js
@@ -1,48 +1,48 @@
 export default [
   {
-    id: '1',
+    id: '1_character',
     familyId: '1_family',
     name: 'Catelyn',
     isDead: true,
     title: 'lady'
   },
   {
-    id: '2',
+    id: '2_character',
     familyId: '1_family',
     name: 'Eddard',
     isDead: true,
     title: 'lord'
   },
   {
-    id: '3',
+    id: '3_character',
     familyId: '1_family',
     name: 'Robb',
     isDead: true,
     title: 'lord'
   },
   {
-    id: '4',
+    id: '4_character',
     familyId: '1_family',
     name: 'Sansa',
     isDead: false,
     title: 'princess'
   },
   {
-    id: '5',
+    id: '5_character',
     familyId: '1_family',
     name: 'Arya',
     isDead: false,
     title: 'princess'
   },
   {
-    id: '6',
+    id: '6_character',
     familyId: '1_family',
     name: 'Bran',
     isDead: false,
     title: 'prince'
   },
   {
-    id: '7',
+    id: '7_character',
     familyId: '1_family',
     name: 'Rickon',
     isDead: false,
@@ -50,187 +50,187 @@ export default [
   },
 
   {
-    id: '8',
+    id: '8_character',
     familyId: '2_family',
     name: 'John',
     isDead: true,
     title: 'lord'
   },
   {
-    id: '9',
+    id: '9_character',
     familyId: '2_family',
     name: 'Lysa',
     isDead: false,
     title: 'lady'
   },
   {
-    id: '10',
+    id: '10_character',
     familyId: '2_family',
     name: 'Robin',
     isDead: false,
     title: 'lord'
   },
   {
-    id: '11',
+    id: '11_character',
     familyId: '3_family',
     name: 'Hoster',
     isDead: true,
     title: 'lord'
   },
   {
-    id: '12',
+    id: '12_character',
     familyId: '3_family',
     name: 'Manisa',
     isDead: true,
     title: 'lady'
   },
   {
-    id: '13',
+    id: '13_character',
     familyId: '3_family',
     husbandFamilyId: '1',
     name: 'Catelyn',
     family: 'Tully'
   },
   {
-    id: '14',
+    id: '14_character',
     familyId: '3_family',
     name: 'Lysa',
     husbandFamilyId: '1'
   },
   {
-    id: '15',
+    id: '15_character',
     familyId: '3_family',
     name: 'Edmure',
     isDead: false,
     title: 'lord'
   },
   {
-    id: '16',
+    id: '16_character',
     familyId: '3_family',
     name: 'Brynden',
     isDead: false,
     title: 'ser'
   },
   {
-    id: '17',
+    id: '17_character',
     familyId: '4_family',
     name: 'Tywin',
     isDead: true,
     title: 'lord'
   },
   {
-    id: '18',
+    id: '18_character',
     familyId: '4_family',
     name: 'Joanna',
     isDead: true,
     title: 'lady'
   },
   {
-    id: '19',
+    id: '19_character',
     familyId: '4_family',
     name: 'Cersei',
     husbandFamilyId: '6'
   },
   {
-    id: '20',
+    id: '20_character',
     familyId: '4_family',
     name: 'Jaime',
     isDead: false,
     title: 'ser'
   },
   {
-    id: '21',
+    id: '21_character',
     familyId: '4_family',
     name: 'Tyrion',
     isDead: false,
     title: null
   },
   {
-    id: '22',
+    id: '22_character',
     familyId: '5_family',
     name: 'Balon',
     isDead: false,
     title: 'lord'
   },
   //{
-  //  id: '23',
+  //  id: '23_character',
   //  familyId: '5_family',
   //  name: 'Alannys',
   //  husbandHouse: 'Harlaw',
   //  family: 'Greyjoy'
   //},
   {
-    id: '24',
+    id: '24_character',
     familyId: '5_family',
     name: 'Rodrik',
     isDead: true,
     title: null
   },
   {
-    id: '25',
+    id: '25_character',
     familyId: '5_family',
     name: 'Maron',
     isDead: true,
     title: null
   },
   {
-    id: '26',
+    id: '26_character',
     familyId: '5_family',
     name: 'Yara',
     isDead: false,
     title: null
   },
   {
-    id: '27',
+    id: '27_character',
     familyId: '5_family',
     name: 'Theon',
     isDead: false,
     title: null
   },
   {
-    id: '28',
+    id: '28_character',
     familyId: '6_family',
     name: 'Robert',
     isDead: true,
     status: 'king'
   },
   {
-    id: '29',
+    id: '29_character',
     familyId: '6_family',
     name: 'Cersei',
     isDead: false,
     status: 'queen'
   },
   {
-    id: '30',
+    id: '30_character',
     familyId: '6_family',
     name: 'Joffrey',
     isDead: true,
     status: 'king'
   },
   {
-    id: '31',
+    id: '31_character',
     familyId: '6_family',
     name: 'Myrcella',
     isDead: true,
     status: 'princess'
   },
   {
-    id: '32',
+    id: '32_character',
     familyId: '6_family',
     name: 'Tommen',
     isDead: false,
     status: 'king'
   },
   {
-    id: '33',
+    id: '33_character',
     familyId: '6_family',
     name: 'Stannis',
     isDead: true,
     status: 'lord'
   },
   {
-    id: '34',
+    id: '34_character',
     familyId: '6_family',
     name: 'Renly',
     isDead: true,
