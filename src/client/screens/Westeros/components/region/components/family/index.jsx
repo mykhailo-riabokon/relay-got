4046f420f2a3ed53a7b5cafc10e5a53311c406e7
mikhail-riabokon <mikhail.riabokon@gmail.com>
2015-11-14 22:16:36 +0200

    layout changes

--- src/client/screens/Westeros/components/region/components/family/index.jsx
+++ src/client/screens/Westeros/components/region/components/family/index.jsx
@@ -19,11 +19,9 @@ export class Family extends Component {
 
   render() {
     let {name, words, id, coatOfArms} = this.props.family;
-    let className = `family ${name && name.toLowerCase() || ''}`;
     let style = {
       backgroundImage: `url(${coatOfArms})`
     };
-
     let memberRoute = new MembersRoute({
       familyId: id,
       regionId: this.props.relay.variables.regionId
@@ -31,10 +29,16 @@ export class Family extends Component {
 
     return (
       <div className="family-container">
-        <div className={className}>
-          <div className="family__name">{name}</div>
+        <div className="family">
+          <div className="info">
+            <div className="label">Rulers:</div>
+            <div className="value">{`House ${name}`}</div>
+          </div>
           <div className="family__coat-of-arms" style={style}></div>
-          <div className="family__words">{words}</div>
+          <div className="info">
+            <div className="label">Words:</div>
+            <div className="value">{words}</div>
+          </div>
         </div>
         <RootContainer Component={Members} route={memberRoute}/>
       </div>
